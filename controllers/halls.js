22010f167db3aad8d2c0afc79781f9405f80079d
Adan Rodriguez <adan.rodriguez.fusta@gmail.com>
2025-09-20 03:12:02 -0300

    add handlers errors

--- controllers/halls.js
+++ controllers/halls.js
@@ -2,31 +2,50 @@ import { HallsModel } from "../models/halls.js";
 
 export class HallsController {
   static getAll = async (req, res) => {
-    const halls = await HallsModel.getAll();
-    res.json(halls);
+    try {
+      const halls = await HallsModel.getAll();
+      res.json(halls);
+    } catch (error) {
+      res.status(500).json(error.message);
+    }
   };
 
   static getById = async (req, res) => {
     const { id } = req.params;
-    const hall = await HallsModel.getById({ id });
-    res.json(hall);
+    try {
+      const hall = await HallsModel.getById({ id });
+      res.json(hall);
+    } catch (error) {
+      res.status(500).json(error.message);
+    }
   };
 
   static create = async (req, res) => {
-    const newHall = await HallsModel.create(req.body);
-    res.status(201).json(newHall);
+    try {
+      const newHall = await HallsModel.create(req.body);
+      res.status(201).json(newHall);
+    } catch (error) {
+      res.status(500).json(error.message);
+    }
   };
 
   static update = async (req, res) => {
     const { id } = req.params;
-    const updatedHall = await HallsModel.update({ id, input: req.body });
-    res.json(updatedHall);
+    try {
+      const updatedHall = await HallsModel.update({ id, input: req.body });
+      res.json(updatedHall);
+    } catch (error) {
+      res.status(500).json(error.message);
+    }
   };
 
   static delete = async (req, res) => {
     const { id } = req.params;
-
-    const result = await HallsModel.delete({ id });
-    res.json(result);
+    try {
+      const result = await HallsModel.delete({ id });
+      res.json(result);
+    } catch (error) {
+      res.status(500).json(error.message);
+    }
   };
 }
